2554ad83928f69380edffd0b15a583a1e37e6c81
minjing <min_jing@aliyun.com>
2018-05-14 16:46:49 +0800

    Now http listener works as expectation

--- funds-flow/server/src/main/java/org/inactionware/fundsflow/action/ListenHttpPort.java
+++ funds-flow/server/src/main/java/org/inactionware/fundsflow/action/ListenHttpPort.java
@@ -3,12 +3,14 @@ package org.inactionware.fundsflow.action;
 import uapi.behavior.ActionIdentify;
 import uapi.behavior.annotation.Action;
 import uapi.behavior.annotation.ActionDo;
-import uapi.net.INetListener;
-import uapi.net.INetListenerInitializer;
-import uapi.net.INetListenerRegistry;
 import uapi.net.http.HttpAttributes;
+import uapi.net.http.IHttpListener;
+import uapi.service.IRegistry;
 import uapi.service.annotation.Inject;
 import uapi.service.annotation.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Action
@@ -17,14 +19,23 @@ public class ListenHttpPort {
     public static final ActionIdentify actionId = ActionIdentify.toActionId(ListenHttpPort.class);
 
     @Inject
-    protected INetListenerRegistry _registry;
+    protected IRegistry _registry;
+
+//    @Inject
+//    protected INetListenerRegistry _registry;
 
     @ActionDo
     public void run(Object input) {
-        INetListenerInitializer listenerInit = this._registry.getInitializer(HttpAttributes.TYPE);
-        listenerInit.setAttribute(HttpAttributes.HOST, "127.0.0.1");
-        listenerInit.setAttribute(HttpAttributes.PORT, 8000);
-        INetListener httpListener = listenerInit.newListener();
+        Map<String, Object> attrs = new HashMap<>();
+        attrs.put(HttpAttributes.HOST, "127.0.0.1");
+        attrs.put(HttpAttributes.PORT, 8000);
+        IHttpListener httpListener = this._registry.findService(IHttpListener.class, attrs);
         httpListener.startUp();
+
+//        INetListenerInitializer listenerInit = this._registry.getInitializer(HttpAttributes.TYPE);
+//        listenerInit.setAttribute(HttpAttributes.HOST, "127.0.0.1");
+//        listenerInit.setAttribute(HttpAttributes.PORT, 8000);
+//        INetListener httpListener = listenerInit.newListener();
+//        httpListener.startUp();
     }
 }
