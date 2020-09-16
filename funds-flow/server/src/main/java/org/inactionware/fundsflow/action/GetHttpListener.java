99c93b45b0fa8f0d6255731283e7087f66b283b5
minjing <min_jing@aliyun.com>
2020-09-16 22:43:11 +0800

    Update code for new API

--- funds-flow/server/src/main/java/org/inactionware/fundsflow/action/GetHttpListener.java
+++ funds-flow/server/src/main/java/org/inactionware/fundsflow/action/GetHttpListener.java
@@ -19,22 +19,19 @@ public class ListenHttpPort {
 
     public static final ActionIdentify actionId = ActionIdentify.toActionId(ListenHttpPort.class);
 
-    @Config(path="server.host")
-    protected String _host;
-
-    @Config(path="server.port")
-    protected int _port;
-
     @Inject
     protected IRegistry _registry;
 
     @ActionDo
-    public void run(Object input) {
+    public IHttpListener listen(
+            final String eventSrc,
+            final String host,
+            final Integer port
+    ) {
         Map<String, Object> attrs = new HashMap<>();
-        attrs.put(HttpAttributes.HOST, this._host);
-        attrs.put(HttpAttributes.PORT, this._port);
-        attrs.put(HttpAttributes.EVENT_SOURCE, "APIServer");
-        IHttpListener httpListener = this._registry.findService(IHttpListener.class, attrs);
-        httpListener.startUp();
+        attrs.put(HttpAttributes.HOST, host);
+        attrs.put(HttpAttributes.PORT, port);
+        attrs.put(HttpAttributes.EVENT_SOURCE, eventSrc);
+        return this._registry.findService(IHttpListener.class, attrs);
     }
 }
